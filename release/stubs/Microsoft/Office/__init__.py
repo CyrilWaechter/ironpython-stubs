860610eef516d07e557933069a48bda7c4891ab0
Nahouhak <cyrwae@hotmail.com>
2018-01-25 07:51:58 +0100

    Starting excel an excel interop module

---
+++ release/stubs/Microsoft/Office/__init__.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+# module Microsoft.Office calls itself Office
+# from Microsoft.Office.Interop.Excel, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c
+# by generator 1.145
+# no doc
+# no imports
+
+# no functions
+# no classes
+# variables with complex values
+
